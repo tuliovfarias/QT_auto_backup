b9135c9cd34dc9d499ed9a5ce51a3dd9b8c83ca5
Túlio Farias <tulio-4ever@hotmail.com>
2022-05-21 00:36:54 -0300

    json add new backups

--- auto_backup/mainwindow.cpp
+++ auto_backup/mainwindow.cpp
@@ -72,13 +72,13 @@ QJsonDocument read_json(QFile file){
     if (file.open(QIODevice::ReadOnly)) {
       QByteArray bytes = file.readAll();
       file.close();
-
-      QJsonParseError jsonError;
-      document = QJsonDocument::fromJson(bytes, &jsonError);
-      if (jsonError.error != QJsonParseError::NoError) {
-        // qInfo() << "fromJson failed: " <<
-        // jsonError.errorString().toStdString() << Qt::endl;
-        throw;
+      if (!bytes.isNull()){
+          QJsonParseError jsonError;
+          document = QJsonDocument::fromJson(bytes, &jsonError);
+          if (jsonError.error != QJsonParseError::NoError) {
+            qDebug() << "Failed while parsing json: " << jsonError.errorString();
+            throw;
+          }
       }
     }
     return document;
@@ -91,36 +91,25 @@ void MainWindow::Button_start_backup_pressed(){
     QJsonObject json_obj = json_doc.object();
 
     foreach(const QString& key, json_obj.keys()) {
-        //qDebug() << json_obj.value(key);
         QJsonValue value = json_obj.value(key);
-        for (const auto& valuew : value.toArray()){
-            //qDebug() << valuew;
-        }
-        //qDebug() << "Key = " << key << ", Value = " << value.toString();
-        qDebug() << "Key = " << key << ", Value = " << value.toArray();
+        qDebug() << "Key= " << key << ", Value= " << value.toArray();
     }
-    //qInfo() << json_obj;
-
     if (ui->list_dest->count() && ui->list_source->count()) {
       QMap<QString, QList<QString>> map;
       for (int i = 0; i < ui->list_dest->count(); ++i) {
         QString path_dest = ui->list_dest->item(i)->text();
+        QJsonArray source_array = json_obj.value(path_dest).toArray();
+        //qDebug() << source_array;
+        //if(source_array.isEmpty()){ //if it is a new destination
         for (int i = 0; i < ui->list_source->count(); ++i) {
           QString path_source = ui->list_source->item(i)->text();
-          map[path_dest].push_back(path_source);
+          source_array.push_back(path_source);
         }
+        json_obj[path_dest] = source_array;
       }
-      // qInfo() << map;
-      QVariantMap vmap;
-      QMapIterator<QString, QList<QString>> i(map);
-      while (i.hasNext()) {
-        i.next();
-        vmap.insert(i.key(), i.value());
-      }
-      // QVariantMap C = vmap.unite(vmap);
-      QJsonDocument new_backup = QJsonDocument::fromVariant(vmap);
-      QByteArray bytes = new_backup.toJson(QJsonDocument::Indented);
-      qInfo() << new_backup;
+      json_doc.setObject(json_obj);
+      QByteArray bytes = json_doc.toJson(QJsonDocument::Indented);
+      qInfo() << json_doc;
 
       if (file.open(QIODevice::ReadWrite)) {
         QTextStream stream(&file);
@@ -130,16 +119,6 @@ void MainWindow::Button_start_backup_pressed(){
       } else {
         ui->footer->setText("File open failed: " + backups_path);
       }
-
-      /*foreach (auto value, map) {
-        qInfo() << value;
-      }*/
-      /*
-      for (auto iter = map.constBegin(); iter != map.constEnd(); ++iter) {
-        QString values = iter.value().join(",");
-        qInfo() << iter.key() << ":" << iter.value().join(",");
-      }*/
-
     } else {
       ui->footer->setText("Source and destination lists cannot be empty!");
     }
@@ -149,5 +128,5 @@ void MainWindow::Button_view_backups_pressed(){
     QString backups_path = default_path+QDir::separator()+"backups.txt";
     QFile file(backups_path);
     QJsonDocument document = read_json(backups_path);
-    qInfo() << document;
+    qDebug() << document;
 }
