b09c7a376cf22d21a3a9bd8360635eda08282fb7
Túlio Farias <tulio-4ever@hotmail.com>
2022-06-05 22:51:13 -0300

    ident file

--- auto_backup/dragdropfilter.cpp
+++ auto_backup/dragdropfilter.cpp
@@ -13,38 +13,38 @@ bool DragDropFilter::eventFilter(QObject* obj, QEvent* event)
     QString selectedSS = QString{"background-color:%1;"}.arg(highlightColor);
 
     switch (event->type()) {
-    case QEvent::DragEnter:
-    {
-        w->setStyleSheet(selectedSS);
-        auto dragEnterEvent = static_cast<QDragEnterEvent*>(event);
-        dragEnterEvent->acceptProposedAction();
-        emit dragEntered(dragEnterEvent->mimeData());
-        break;
-    }
-    case QEvent::DragMove:
-    {
-        auto dragMoveEvent = static_cast<QDragMoveEvent*>(event);
-        dragMoveEvent->acceptProposedAction();
-        break;
-    }
-    case QEvent::DragLeave:
-    {
-        w->setStyleSheet("");
-        auto dragLeaveEvent = static_cast<QDragLeaveEvent*>(event);
-        dragLeaveEvent->accept();
-        emit dragDeparted();
-        break;
-    }
-    case QEvent::Drop:
-    {
-        w->setStyleSheet("");
-        auto dropEvent = static_cast<QDropEvent*>(event);
-        dropEvent->acceptProposedAction();
-        emit dragDropped(dropEvent->mimeData());
-        break;
-    }
-    default:
-        return QObject::eventFilter(obj, event);
+        case QEvent::DragEnter:
+        {
+            w->setStyleSheet(selectedSS);
+            auto dragEnterEvent = static_cast<QDragEnterEvent*>(event);
+            dragEnterEvent->acceptProposedAction();
+            emit dragEntered(dragEnterEvent->mimeData());
+            break;
+        }
+        case QEvent::DragMove:
+        {
+            auto dragMoveEvent = static_cast<QDragMoveEvent*>(event);
+            dragMoveEvent->acceptProposedAction();
+            break;
+        }
+        case QEvent::DragLeave:
+        {
+            w->setStyleSheet("");
+            auto dragLeaveEvent = static_cast<QDragLeaveEvent*>(event);
+            dragLeaveEvent->accept();
+            emit dragDeparted();
+            break;
+        }
+        case QEvent::Drop:
+        {
+            w->setStyleSheet("");
+            auto dropEvent = static_cast<QDropEvent*>(event);
+            dropEvent->acceptProposedAction();
+            emit dragDropped(dropEvent->mimeData());
+            break;
+        }
+        default:
+            return QObject::eventFilter(obj, event);
     }
 
     return true;
